540439be2957a7f6bb260f79ea5d06916021e4c7
Tyler Cowie <tylercowie@users.noreply.github.com>
2023-07-01 21:20:14 -0500

    Update app.js

--- my-website/pages/app.js
+++ my-website/pages/app.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import '../styles/globals.css';
+///import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
